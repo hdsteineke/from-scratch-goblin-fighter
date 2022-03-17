8e902d9331c4ba9b0df073d95e2ceb6d35028493
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-17 12:18:19 -0700

    wrote renderGoblin function

---
+++ render-utils.js
@@ -0,0 +1,21 @@
+export function renderGoblin(goblin) {
+    const goblinEl = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const hpEl = document.createElementNS('p');
+
+    let goblinEmoji = '';
+
+    if (goblin.hp >= 4) {
+        goblinEmoji = '👹';
+    } else if (goblin.hp === 0) {
+        goblinEmoji = '💀';
+    }
+
+    nameEl.textContent = goblin.name;
+    hpEl.textContent = `${goblinEmoji} ${goblin.hp}`;
+
+    goblinEl.append(nameEl, hpEl);
+
+    return goblinEl;
+
+}
