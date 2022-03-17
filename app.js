0acdca1c07a545bb79e24bf8a357fc59078c8303
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-17 12:00:02 -0700

    grabbed DOM elements and set state

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // import functions and grab DOM elements
+const spanDefeated = document.querySelector('#defeated-goblins');
+const spanHP = document.querySelector('#player-hp');
+
+const goblinInput = document.querySelector('#goblin-input');
+const goblinButton = document.querySelector('#spawn-goblin-button');
+const goblinForm = document.querySelector('form');
+const goblinList = document.querySelector('.goblin-container');
 
 // let state
+let defeatedGoblins = 0;
+let playerHP = 10;
+let goblins = [
+    { name: 'Fabian', hp: 3 },
+    { name: 'Flupe', hp: 4 },
+    { name: 'Jimbo', hp: 2 }
+];
 
 // set event listeners 
   // get user input
